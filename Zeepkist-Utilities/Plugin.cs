ff56057b3d7f92d2f032b46c595d86f6a4022743
Christiaan Bloemendaal <github@tnrd.net>
2022-10-12 20:07:48 +0200

    Added initial project

---
+++ Zeepkist-Utilities/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace TNRD.Zeepkist.Utilities
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony;
+        
+        private void Awake()
+        {
+            harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            gameObject.AddComponent<ClickScriptDisabler>();
+            
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
---
+++ Zeepkist-Utilities/Utilities/ClickScriptDisabler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace TNRD.Zeepkist.Utilities;
+
+public sealed class ClickScriptDisabler : MonoBehaviour
+{
+    private static LEV_ClickScript clickScript;
+    private static int disableRequests;
+
+    [PublicAPI] public static bool IsEnabled => disableRequests == 0;
+
+    [PublicAPI]
+    public static void Enable()
+    {
+        disableRequests = Math.Max(0, disableRequests - 1);
+        UpdateClickScript();
+    }
+
+    [PublicAPI]
+    public static void Disable()
+    {
+        disableRequests++;
+        UpdateClickScript();
+    }
+
+    private static void FetchLatestInstance()
+    {
+        if (clickScript == null)
+            clickScript = FindObjectOfType<LEV_ClickScript>();
+    }
+
+    private static void UpdateClickScript()
+    {
+        FetchLatestInstance();
+
+        if (clickScript != null)
+        {
+            clickScript.enabled = IsEnabled;
+        }
+        else
+        {
+            Debug.LogWarning("Trying to update ClickScript's state but we don't have an instance!");
+        }
+    }
+}
